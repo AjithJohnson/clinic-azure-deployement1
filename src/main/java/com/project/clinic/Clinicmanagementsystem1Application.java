c6816edb8f1844b031d552f5dee82f02138a9e05
Ajith Johnson <99463897+AjithJohnson@users.noreply.github.com>
2022-04-13 14:20:25 +0530

    Add files via upload

---
+++ src/main/java/com/project/clinic/Clinicmanagementsystem1Application.java
@@ -0,0 +1,13 @@
+package com.project.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clinicmanagementsystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clinicmanagementsystem1Application.class, args);
+	}
+
+}
